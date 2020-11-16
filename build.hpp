8b3158ba4126b6b6650e6e3c6e365bbb610b573c
Gregory Talotta <gjtalotta@alaska.edu>
2020-11-15 23:32:43 -0900

    took in the hight and width

--- build.hpp
+++ build.hpp
@@ -1,14 +1,14 @@
-// build.h
+// build.hpp
 // Greg Talotta
 // 15 Nov 2020
 //
 // For CS 411 Fall 2020
 // Used in Assignment 5
-#ifndef FILE_BUILD_H_INCLUDED
-#define FILE_BUILD_H_INCLUDED
+#ifndef FILE_BUILD_HPP_INCLUDED
+#define FILE_BUILD_HPP_INCLUDED
 #include <vector>
 #include <algorithm>
 using Bridge = std::vector<int>;
 
 int build(int w, int e, const std::vector<Bridge> &bridges);
-#endif //#ifndef FILE_BUILD_H_INCLUDED
+#endif //#ifndef FILE_BUILD_HPP_INCLUDED
